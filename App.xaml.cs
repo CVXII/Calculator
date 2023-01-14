3893f9974e995af475486e233ca12908aaf98a27
CVXII <113898921+CVXII@users.noreply.github.com>
2023-01-14 09:18:35 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace MauiScientificCalculator;
+
+public partial class App : Application
+{
+    public App()
+	{
+		InitializeComponent();
+
+
+        MainPage = new CalculatorPage();
+	}
+}
---
+++ ViewModels/PageViewModel.cs
@@ -0,0 +1,112 @@
+﻿namespace MauiScientificCalculator.ViewModels;
+
+[INotifyPropertyChanged]
+internal partial class PageViewModel
+{
+    [ObservableProperty]
+    private string inputText = "";
+
+    [ObservableProperty]
+    private string calculatedResult = "0";
+
+    private bool isSciOpWaiting = false;
+
+    public PageViewModel()
+    {
+    }
+
+    [RelayCommand]
+    private void Reset()
+    {
+        CalculatedResult = "0";
+        InputText = "";
+        isSciOpWaiting = false;
+    }
+
+    [RelayCommand]
+    private void Calculate()
+    {
+        if (InputText.Length == 0)
+            return;
+
+        if (isSciOpWaiting)
+        {
+            InputText += ")";
+            isSciOpWaiting = false;
+        }
+
+        try
+        {
+            var inputString = NormalizeInputString();
+            var expression = new Expression(inputString);
+            var result = expression.Evaluate();
+
+            CalculatedResult = result.ToString();
+        }
+        catch (Exception ex)
+        {
+            CalculatedResult = "";
+        }
+    }
+
+    private string NormalizeInputString()
+    {
+        Dictionary<string, string> _opMapper = new()
+        {
+            {"×", "*"},
+            {"÷", "/"},
+           
+        };
+
+        var retString = InputText;
+
+        foreach (var key in _opMapper.Keys)
+        {
+            retString = retString.Replace(key, _opMapper[key]);
+        }
+
+        return retString;
+    }
+
+    [RelayCommand]
+    private void Backspace()
+    {
+        if (InputText.Length > 0)
+            InputText = InputText.Substring(0, InputText.Length - 1);
+    }
+
+    [RelayCommand]
+    private void NumberInput(string key)
+    {
+        InputText += key;
+    }
+
+    [RelayCommand]
+    private void MathOperator(string op)
+    {
+        if (isSciOpWaiting)
+        {
+            InputText += ")";
+            isSciOpWaiting = false;
+        }
+
+        InputText += $" {op} ";
+    }
+
+    [RelayCommand]
+    private void RegionOperator(string op)
+    {
+        if (op == ")")
+            isSciOpWaiting = false;
+
+        InputText += op;
+    }
+
+    [RelayCommand]
+    private void ScientificOperator(string op)
+    {
+        InputText += $"{op}(";
+        isSciOpWaiting = true;
+    }
+
+}
